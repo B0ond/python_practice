cb5adb500e5f58044d72f5d235a514584557efdf
B0ond <ivan500012@yandex.ru>
2023-04-18 21:37:24 +0300

    Stepik 7.5 python

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ summa = 0
 count = 0
 myltiply = 1
 avgarif = 0
-firstdigit = 0
+num_str = str(n)
+firstdigit = num_str[0]                # первую цифру
+last_digit = n % 10
 while n:
     lastdigit = n % 10
-    summa += lastdigit        # Сумма цифр
-    count += 1                # киличество цифр
-    myltiply *= lastdigit     # произведение цифр
-    avgarif = summa / count   # среднее арифметическое
+    summa += lastdigit                 # Сумма цифр
+    count += 1                         # киличество цифр
+    myltiply *= lastdigit              # произведение цифр
+    avgarif = summa / count            # среднее арифметическое
     n = n//10
-for i in range count
+summa_2 = int(firstdigit) + last_digit # сумму первой и последней
 
-
-
-# первую цифру
-# сумму первой и последней
 print(summa)
 print(count)
 print(myltiply)
 print(avgarif)
 print(firstdigit)
+print(summa_2)
 
