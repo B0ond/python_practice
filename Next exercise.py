83c6decdaa436f9965ef7bd93612403c396c971e
amiram <amiram.yalaltdinov@tatar.ru>
2023-04-20 10:14:53 +0300

    commit from op-1 -2

--- Next exercise.py
+++ Next exercise.py
@@ -7,8 +7,9 @@ if n >= 10:
         quantity += 1
         n = n // 10
 second_digit = (n2 // 10 ** (quantity - 2) % 10)
-print("Второе число = ", second_digit,"наше второе n = ", n2)
+print("Второе число = ", second_digit,"n2 = ", n2)
 print("Количество = ", quantity)
-print("Н деленый на 10 = ", n2 // 10)
-print("количество -2 = ", quantity -2)
-print("возведенное в степень", n2 // 10 ** 4)
+print("n2 // 10 = ", n2 // 10)
+print("quantity -2 = ", quantity -2)
+print("возведенное в степень", n2 // 10 ** quantity)
+print("10 в степени которую указал", 10 ** quantity)
