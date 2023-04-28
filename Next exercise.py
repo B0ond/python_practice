6b4c9b6df08727d457ebd0737dcb8021c4e50641
Amir <amiram19@yandex.ru>
2023-04-28 15:41:49 +0300

    Next 2

--- Next exercise.py
+++ Next exercise.py
@@ -1,5 +1,5 @@
 n = int(input())
-while n % 10 >= n // 10 % 10 or n > 0:
-    n = n // 10
-print('Yes' if n < 10 else 'NO')
-#???
+rsoult = 0
+for i in range(2, n+1):
+    if rsoult == n % i:
+        print(rsoult)
