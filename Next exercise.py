b7a62d8d997da4733382f72132f4c96b61ff1ba5
Amir <amiram19@yandex.ru>
2023-05-04 08:58:14 +0300

    review code 7.7 stepic

--- Next exercise.py
+++ Next exercise.py
@@ -1,5 +1,12 @@
 n = int(input())
-rsoult = 0
-for i in range(2, n+1):
-    if rsoult == n % i:
-        print(rsoult)
+max_digit = 0
+for _ in range(n):
+    last_digit = n % 10
+    if last_digit % 3 == 0:
+        if last_digit > max_digit:
+            max_digit == last_digit
+    n = n % 10
+if max_digit == 0:
+    print('NO')
+else:
+    print(max_digit)
--- Temporary_plase.py
+++ Temporary_plase.py
@@ -1,1 +1,12 @@
-print(15 % 4)
+n = int(input())
+max_digit = n % 10
+while n > 0:
+    last_digit = n % 10
+    if digit % 3 == 0:
+        if digit < max_digit:
+            digit = max_digit
+    n = n % 10
+if max_digit == 0:
+    print('NO')
+else:
+    print(max_digit)
