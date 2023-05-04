67f8f6dc6b5543f43f99e0cd446af3e374fbf636
amiram <amiram.yalaltdinov@tatar.ru>
2023-05-04 10:44:10 +0300

    commit from op-1 -2

--- Next exercise.py
+++ Next exercise.py
@@ -1,10 +1,10 @@
 n = int(input())
-max_digit = 0
-for _ in range(n):
-    last_digit = n % 10
-    if last_digit % 3 == 0:
-        if last_digit > max_digit:
-            max_digit == last_digit
+max_digit = n % 10
+while n > 0:
+    digit = n % 10
+    if digit % 3 == 0:
+        if digit < max_digit:
+            digit = max_digit
     n = n % 10
 if max_digit == 0:
     print('NO')
--- Temporary_plase.py
+++ Temporary_plase.py
@@ -1,12 +1,12 @@
 n = int(input())
-max_digit = n % 10
-while n > 0:
-    last_digit = n % 10
-    if digit % 3 == 0:
-        if digit < max_digit:
-            digit = max_digit
-    n = n % 10
-if max_digit == 0:
+max_digit = -1                        #0 делится на любое натуральное чилсо поэтому граница будет от -1
+while n > 0:                          #пока не кончится
+    last_digit = n % 10               #переименовал переменную по PeP
+    if last_digit % 3 == 0:
+        if last_digit > max_digit:    #для перезаписси условия недо поставить >
+            max_digit = last_digit    # мы приравниваем переменную max_digit к последней а не наоборот
+    n = n // 10                       #убераем вычисленное число для что бы начать следующее
+if max_digit == -1:                   #нсли неменяется установленный msx_digit то пишем NO
     print('NO')
 else:
     print(max_digit)
