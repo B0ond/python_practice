35f406f0fae9e01c9501d019f9f9e9515b0239eb
Amir <amiram19@yandex.ru>
2023-05-05 15:25:59 +0300

    review code 7.8 stepic

--- Next exercise.py
+++ Next exercise.py
@@ -1,12 +1,5 @@
 n = int(input())
-max_digit = n % 10
-while n > 0:
-    digit = n % 10
-    if digit % 3 == 0:
-        if digit < max_digit:
-            digit = max_digit
-    n = n % 10
-if max_digit == 0:
-    print('NO')
-else:
-    print(max_digit)
+for _ in range(n):
+    for j in range(j):
+        print(n, end=' ')
+    print()
