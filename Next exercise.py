25fcf60ee0f90a492dae1b407e5859854f356f92
B0ond <amiram19@yandex.ru>
2023-05-09 15:10:12 +0300

    7.9 stepic

--- Next exercise.py
+++ Next exercise.py
@@ -1,2 +1,9 @@
-from math import pi
-print([str(round(pi, i)) for i in range(1, 6)])
+n = int(input())
+nx = n
+if n % 2 > 0:
+    for i in range(1, n // 2 + 1):
+        print("*" * i)
+    for j in range(nx // 2 + 1, 0, -1):
+        print("*" * j)
+else:
+    print('Not a odd number')
