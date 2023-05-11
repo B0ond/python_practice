23c5a1d96c8d50e2e7e3f20d9b1cb6aff7873ccf
amiram <amiram.yalaltdinov@tatar.ru>
2023-05-11 16:06:13 +0300

    7.8 done stepic 222

--- Next exercise.py
+++ Next exercise.py
@@ -1,6 +1,9 @@
 # put your python code here
-for n in range(1, 14):
-    for k in range(1, 13):
-        for m in range(1, 13):
-            if 28 * n + 30 * n + 31 * m == 356:
-                print('n =', n, 'k=', k, 'm =', m )
+total = 0
+for x in range(1, 10):
+    for y in range(1, 20):
+        for j in range(1, 200):
+            if 10 * x + 5 * y + 0.5 * j == 100:
+                total += 1
+                print('Быки', x, 'коровы', y, 'телята', j )
+                print('Общее количество решений',total)
