b83834d709cdbcaa83207feff0b441151fa72fbc
Amir <amiram19@yandex.ru>
2023-08-03 15:48:23 +0300

    Self learning, string and loop 0.2

--- file1.py
+++ file1.py
@@ -1,11 +1,6 @@
-file = open('file.txt', encoding='utf-8')
-text_length = len(file.read())
-if text_length < 100:
-    print('Number of characters less than 100, it is:', text_length)
-else:
-    print('there are more than 100 characters here, they are here', text_length, end='')
-# print(file.read(100))
+import random
 
-# for row in file:
-#     for letter in row():
-#         print(letter, end='')
+dataformat = ['asdasd', 'bbbbbbbbb', 'cccccccccc', 'ddddddddddd', 'eeeeeeeeeeeee']
+newobject = random.randint(1, len(dataformat))
+dataformat.append(dataformat[newobject])
+print(dataformat)
