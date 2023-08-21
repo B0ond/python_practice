58ff3f8f7f7aee2137bd7c6348a22a11488f0f88
Amir <amiram19@yandex.ru>
2023-08-21 16:45:41 +0300

    Self learning, string and loop 0.3

--- file1.py
+++ file1.py
@@ -1,6 +1,9 @@
 import random
 
-dataformat = ['asdasd', 'bbbbbbbbb', 'cccccccccc', 'ddddddddddd', 'eeeeeeeeeeeee']
-newobject = random.randint(1, len(dataformat))
-dataformat.append(dataformat[newobject])
-print(dataformat)
+# dataformat = ['asdasd', 'bbbbbbbbb', 'cccccccccc', 'ddddddddddd', 'eeeeeeeeeeeee']
+# newobject = random.randint(1, len(dataformat))
+# dataformat.append(dataformat[newobject])
+# print(dataformat)
+
+newobject = random.randint(1, 10)
+print(type(newobject), newobject, sep='\n')
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-matrix = [
-    [1, 2, 3, 6, 7],
-    [22, 56, 76, 34, 34],
-    [2.5, 7.5, 7.2, 2.7, 4.4],
-    ['a', 'b', 'c', 'd', 'e']
-]
+# matrix = [
+#     [1, 2, 3, 6, 7],
+#     [22, 56, 76, 34, 34],
+#     [2.5, 7.5, 7.2, 2.7, 4.4],
+#     ['a', 'b', 'c', 'd', 'e']
+# ]
+matrix = [1, 2, 3, 6, 7]
+
 
 transposed_matrix = [[transp[i] for transp in matrix]for i in range(4)]
 print(transposed_matrix)
