c36637d1a7ee244ecb538c2c3092888c7293f9b0
Amir <amiram19@yandex.ru>
2023-08-22 16:35:17 +0300

    Self learning, string and loop 0.5

--- Temporary_plase.py
+++ Temporary_plase.py
@@ -3,6 +3,6 @@ if ages.count(max(ages)) == 1:
     print(names[ages.index(max(ages))], 'старше всех')
 elif ages.count(max(ages)) == 2:
     index = ages.index(max(ages))
-    print(names[index], 'и', names[ages.index(max(ages), index + 1)], 'старше всех')            # задаем перенос строки
+    print(names[index], 'и', names[ages.index(max(ages), index + 1)], 'старше всех')
 elif ages.count(max(ages)) == 3:
     print('Все одинаковы')
