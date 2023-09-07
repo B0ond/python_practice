20a0dfdbed430962eb5f280617abdadf0738c01a
Amir <amiram19@yandex.ru>
2023-09-07 08:36:01 +0300

    Learn book page 80, 99 bottles

--- Next exercise.py
+++ Next exercise.py
@@ -1,2 +1,13 @@
-from datetime import datetime
-odds = [1, 3]
+word = 'Бутылок'
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "пива на стене")
+    print(beer_num, word, 'пива.')
+    print("Возьми одну")
+    print('Пусти по кругу')
+    if beer_num > 1:
+        if beer_num >
+        print(beer_num - 1, word, 'пива на стене')
+    elif beer_num == 1:
+        word = 'бутылка'
+        print(beer_num - 1, word, 'пива на стене')
+    print()
