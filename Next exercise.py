1e00a52d301cbf73a2c832ccb35f37f28903ea72
amiram <amiram.yalaltdinov@tatar.ru>
2023-09-07 10:54:22 +0300

    75 page from learning book

--- Next exercise.py
+++ Next exercise.py
@@ -1,13 +1,21 @@
-word = 'Бутылок'
+word = 'бутылок'
 for beer_num in range(99, 0, -1):
     print(beer_num, word, "пива на стене")
-    print(beer_num, word, 'пива.')
+    print(beer_num, word, "пива!")
     print("Возьми одну")
-    print('Пусти по кругу')
-    if beer_num > 1:
-        if beer_num >
-        print(beer_num - 1, word, 'пива на стене')
-    elif beer_num == 1:
-        word = 'бутылка'
-        print(beer_num - 1, word, 'пива на стене')
+    print("Пусти по кругу")
+    new_num = beer_num - 1
+    if new_num == 0:
+        print("Кончились бутылки пива на стене")
+        exit()
+    else:
+        if 11 <= beer_num <= 19:
+            word = 'бутылок'
+        elif beer_num % 10 == 1:
+            word = 'бутылка'
+        elif beer_num % 10 in (2, 3, 4):
+            word = 'бутылки'
+        else:
+            word = "бутылок"
+    print(new_num, word, "пива на стене")
     print()
