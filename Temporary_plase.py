8045190fb985352552a5dacc1660368290c236c9
Amir <amiram19@yandex.ru>
2023-09-12 15:19:28 +0300

    Learn book page 80, 99 bottles, fix

--- Temporary_plase.py
+++ Temporary_plase.py
@@ -1,3 +1,14 @@
-import time
-print(time.strftime("%I:%M"))
-print(time.strftime("%A %p"))
+a = {'name'}
+b = {'name', 'age'}
+c = {'name', 'age', 1, 2, 3.5}
+d = {'name':'Avgust', 'age':65}
+e = {'name':'Avgust', 'age':88, 1:34, 2:45, 3.5:56}
+list_of_sets = [a, b, c, d, e]
+names = ['a', 'b', 'c', 'd', 'e']
+for i, item in enumerate(list_of_sets):
+    print(names[i], type(item))
+    # i +=1
+# i = 0
+# for item in list_of_sets:
+#     print(names[i], type(item))
+#     i +=1
