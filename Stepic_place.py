0dee47fd92569a60fbfa31f9039e7edf6672edc6
amiram <amiram.yalaltdinov@tatar.ru>
2023-09-21 14:20:41 +0300

    some code to stepic

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,5 +1,26 @@
-for x in range(1, 10):
-    for y in range(1, 18):
-        for z in range(2, 99, 2):
-            if (x*10 + y*5 + z*0.5 == 100) and (x + y + z == 100):
-                print(x, y, z)
+# wins = 0
+# lose = 0
+# for a in range(1, 10):
+#     for b in range(1, 18):
+#         for c in range(1, 100):
+#             for d in range(1, 100):
+#                 for e in range(1, 150):
+#                     if (a**5 + b**5 + c**5 + d**5 == e**5):
+#                         wins += 1
+#                     else:
+#                         lose += 1
+# print('Верных решений = ', wins)
+# print("Недачных попыток = ", lose)
+wins = 0
+lose = 0
+for a in range(1, 150):
+    for b in range(1, 150):
+        for c in range(1, 150):
+            for d in range(1, 150):
+                e = (a ** 5 + b ** 5 + c ** 5 + d ** 5) ** (1/5)
+                if e.is_integer():
+                    wins += 1
+                else:
+                    lose += 1
+print('Верных решений =', wins)
+print('Недачных попыток =', lose)
