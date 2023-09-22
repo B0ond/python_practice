ad948e3628fdbd33566ca5e3ef20c67af4cb2f32
Amir <amiram19@yandex.ru>
2023-09-22 10:51:45 +0300

    stepic python course chapter 7.8

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,5 +1,24 @@
-for x in range(1, 10):
-    for y in range(1, 18):
-        for z in range(2, 99, 2):
-            if (x*10 + y*5 + z*0.5 == 100) and (x + y + z == 100):
-                print(x, y, z)
+#a5+b5+c5+d5 =e5
+from datetime import datetime
+start_time = datetime.now()
+summa = 0
+power = 5
+loos = 0
+wins = 0
+for a in range(1, 150):
+    for b in range(1, 150):
+        for c in range(1, 150):
+            for d in range(1, 150):
+                loos += 1
+                if a**power + b**power + c**power == d**power:
+                    wins += 1
+                    # print('a =', a, 'b =', b, 'c =', c, 'd =', d, end=' ')
+                break
+summa = a+b+c+d
+print('a = ', a, 'b = ', b, 'c = ', c, 'd = ', d, end='\n')
+print('Верных решений = ', wins)
+print('Неверных решений = ', loos)
+
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
+#print('сумма верных решений = ', summa)
