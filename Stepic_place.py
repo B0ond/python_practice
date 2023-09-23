371711a99a3b4288b2b4583ada1d2e216215060c
B0ond <amiram19@yandex.ru>
2023-09-24 01:22:02 +0300

    final homework stepic

--- Stepic_place.py
+++ Stepic_place.py
@@ -4,21 +4,26 @@ start_time = datetime.now()
 summa = 0
 power = 5
 loos = 0
-wins = 0
-for a in range(1, 150):
-    for b in range(1, 150):
-        for c in range(1, 150):
-            for d in range(1, 150):
-                loos += 1
-                if a**power + b**power + c**power == d**power:
-                    wins += 1
-                    # print('a =', a, 'b =', b, 'c =', c, 'd =', d, end=' ')
+found_solution = False
+for a in range(1, 151):
+    if found_solution == True:
+        break
+    for b in range(1, 151):
+        if found_solution == True:
+            break
+        for c in range(1, 151):
+            if found_solution == True:
                 break
-summa = a+b+c+d
-print('a = ', a, 'b = ', b, 'c = ', c, 'd = ', d, end='\n')
-print('Верных решений = ', wins)
-print('Неверных решений = ', loos)
-
+            for d in range(1, 151):
+                sum = a ** power + b ** power + c ** power + d ** power
+                e = int(sum ** (0.2))
+                if sum == e ** 5:
+                    print('a =', a, 'b =', b, 'c =', c, 'd =', d,'e =', e)
+                    print('Искомое число =', a+b+c+d+e)
+                    found_solution = True
+                    break
+                else:
+                    loos += 1
 end_time = datetime.now()
+print("Неудачных решений =", loos)
 print('Duration: {}'.format(end_time - start_time))
-#print('сумма верных решений = ', summa)
