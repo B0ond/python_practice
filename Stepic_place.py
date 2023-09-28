f10b508593b351f09ee49a5a594a9317f4b748f7
amiram <amiram.yalaltdinov@tatar.ru>
2023-09-28 11:28:06 +0300

    some code to stepic

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,9 +1,9 @@
-a = int(input())
-count = 0
-temp = 0
-while a >= 1:
-    temp = a % 10
-    count += temp
-    a = a // 10
-    while count
-print(count)
+a, b = int(input()), int(input())
+while a < 2:
+    a += 1
+prime = 0
+for i in range(a, b+1):
+    if i == 2 or i == 3 or i == 5 or i == 7 or i == 11:
+        print(i)
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0 and i % 11 != 0 and i % (i ** (0.2)) != 0:
+        print(i)
