05a95e36952564e2942030c57339d521a42f924e
B0ond <amiram19@yandex.ru>
2023-10-02 21:10:44 +0300

    Revert "Revert "stepic exercises""
    This reverts commit d539a1c11466c15c86a1cb81b44791ecfe5ff451.

--- Temporary_plase.py
+++ Temporary_plase.py
@@ -1,23 +1,5 @@
-phrase = "Don't panic!"
-plist = list(phrase)
-print(phrase)
-print(plist)
-for i in range(1):
-    plist.pop(i)
-plist.remove("'")
-for _ in range(4):
-    plist.pop()
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-# t = plist.pop(2)
-# p = plist.pop(3)
-# plist.insert(3, t)
-# plist.insert(5, p)
-
-
-
-new_phrase = ''.join(plist)
-print(plist)
-print(new_phrase)
-
-#target = 'on tap'
+data = []
+with open('file.txt', encoding='UTF-8') as file:
+    for lines in file:
+        data = lines.lstrip()
+print(list(data))
