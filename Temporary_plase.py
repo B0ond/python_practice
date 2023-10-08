db1053e541235f6cf04e123e83f011c615287a67
B0ond <amiram19@yandex.ru>
2023-10-08 18:10:11 +0300

    stepic exercises

--- Temporary_plase.py
+++ Temporary_plase.py
@@ -1,5 +1,8 @@
-data = []
-with open('file.txt', encoding='UTF-8') as file:
-    for lines in file:
-        data = lines.lstrip()
-print(list(data))
+cipher_shift = int(input())
+shift_message = input()
+
+for i in shift_message:
+    shift_char = ord(i) - cipher_shift
+    if  shift_char < 97:
+        shift_char += 26
+    print(chr(shift_char), end='')
