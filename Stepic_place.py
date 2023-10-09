a2f6634d919151c9abf2882b99ec8e0ea6ec275b
B0ond <amiram19@yandex.ru>
2023-10-09 22:02:43 +0300

    stepic exercises

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,10 +1,13 @@
-x = input()
+n = int(input())
+spisok = []
+stop_num = 0
 
+for i in range(n):
+    x = input()
+    if len(x) > 2:
+        spisok.append(x[1])
+    else:
+        continue
+result_str = ''.join(spisok)
 
-start = x.find('h')
-end = x.rfind('h')
-
-rep_x = x[start+1:end]
-rev_x = rep_x[::-1]
-
-print(x.replace(rep_x, rev_x))
+print(result_str)
--- Stepic_place_2.py
+++ Stepic_place_2.py
@@ -1,8 +1,1 @@
-num = int(input())
-сounter = 0
-
-for _ in range(num):
-    stroka = input()
-    if stroka.count('11') >= 3:
-        сounter += 1
-print(сounter)
+print(len('eee')>3)
