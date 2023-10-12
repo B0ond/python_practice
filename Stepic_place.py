fd78764b0f4bdda85705d908ff31c792f548a94c
amiram <amiram.yalaltdinov@tatar.ru>
2023-10-12 10:59:35 +0300

    some code to stepicff

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,7 +1,15 @@
-spisok = [input() for _ in range(int(input()))]
+number_of_rows = int(input())
+spisok = [input() for _ in range(number_of_rows)]
 requests = [input() for _ in range(int(input()))]
 
 out = []
-for i in spisok:
-    if requests.lower() in i.lower():
-        print(i)
+for i, j in enumerate(spisok):
+    # for item in requests:
+    #     if item in spisok:
+    if all([item for item in requests if item in spisok]):
+        print(j)
+
+
+# for i in range(number_of_rows):
+#     if requests[i].lower() in spisok[i].lower():
+#         print(i)
