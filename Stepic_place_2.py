747bf79c58e40c238021ed29ec7c3d693ec38fbd
B0ond <amiram19@yandex.ru>
2023-10-23 22:05:14 +0300

    stepic description and decoding text

--- Stepic_place_2.py
+++ Stepic_place_2.py
@@ -1,6 +1,10 @@
 stroka = input().split()
-print(*[i. for i in stroka if len(i) > 1])
 
+deleted_ki = ([i[:-2] for i in stroka if len(i) > 1])
+decoded_text = ([i[-1]+i[:-1] for i in deleted_ki if len(i) > 1])
+# if 'ки' in decoded_text >= len(stroka) / 2 >= 10:
+
+print(*decoded_text)
 
 
 # stroka = input().split()
