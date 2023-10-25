9110b5a0d7ab6ad33b1d08dc72f678912e1e20ee
B0ond <amiram19@yandex.ru>
2023-10-25 21:37:48 +0300

    create pet project dir and 2 files

--- Pet_projects/encryption-decription_text/decryption text.py
+++ Pet_projects/encryption-decription_text/decryption text.py
@@ -4,11 +4,3 @@ deleted_ki = ([i[:-2] for i in stroka if len(i) > 1])
 decoded_text = ([i[-1]+i[:-1] for i in deleted_ki if len(i) > 1])
 
 print(*decoded_text)
-
-
-# stroka = input().split()
-# first_str = [i[0] for i in stroka]
-# print(first_str)
-#
-# a ='животное'
-# print(a[:3])
