77d7da1f43b5d6cba64fe3e827d0d0f93a47c1a2
B0ond <amiram19@yandex.ru>
2023-11-12 19:43:54 +0300

    create pet project dir and 2 filesваппвып

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,7 +1,15 @@
-def is_valid_triangle(side1, side2, side3):
-    return 'YES' if a + b > c and a + c > b and b + c > a else 'NO'
+def is_palindrome(text):
+    txt_lower = []
+    for i in text:
+        if i not in [' ', ',', '.', '!', '?', '-']:
+            txt_lower.append(i.lower())
+    pure_text = ''.join(txt_lower)
+    print('pure text', pure_text)
+    print('reversed text', pure_text[::-1])
+    return pure_text == pure_text[::-1]
+    # for j in range(pure_text):
 
-# считываем данные
-a, b, c = int(input()), int(input()), int(input())
+txt = input()
 
-print(is_valid_triangle(a, b, c))
+
+print(is_palindrome(txt))
