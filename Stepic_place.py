c613e176b11eec15126a9de4956ebd5a2e6152f7
B0ond <amiram19@yandex.ru>
2023-11-13 21:49:05 +0300

    create pet project dir and 2 kkkkвапва

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,50 +1,12 @@
-def is_palindrome(num):
-    return num == num[::-1]
+def convert_to_python_case(text):
+    for i in range(1,len(text)):
+        if text[i].isupper():
+            text = text[:i] + '_' + text[i].lower() + text[i+1:]
+    text = text.lower()
+    return text
 
-def is_prime(num):
-    num = int(num)
-    if num == 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+# считываем данные
+txt = input()
 
-def is_even(num):
-    num = int(num)
-    if num % 2 == 0:
-        return True
-
-def good_pass(paswrd):
-    out = False
-    indx = []
-    count = 0
-
-    for i in range(len(paswrd)):
-        if paswrd[i] == ':':
-            count += 1
-            indx.append(i)
-            if count > 2:
-                return False
-    if count == 2:
-        if is_palindrome(paswrd[:indx[0]]):
-            out = True
-        else:
-            return False
-        if is_prime(paswrd[indx[0]+1:indx[1]]):
-            out = True
-        else:
-            return False
-        if is_even(paswrd[indx[1]+1:]):
-            out = True
-        else:
-            return False
-    if out == True:
-        return True
-
-
-
-num = input()
-
-
-print(good_pass(num))
+# вызываем функцию
+print(convert_to_python_case(txt))
