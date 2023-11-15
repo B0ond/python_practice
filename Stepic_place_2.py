b274631d446cbed9b1429c6b8e9ed6657c0c0475
Amir <amiram19@yandex.ru>
2023-11-15 13:48:42 +0300

    stepic python course 11.6fdg fffgddsdfg

--- Stepic_place_2.py
+++ Stepic_place_2.py
@@ -1,7 +1,10 @@
 def is_magic(date):
     date_list = date.split('.')
-    day_and_moth = date_list[0] * date_list[1]
-    if day_and_moth ==
+    day_and_moth = int(date_list[0]) * int(date_list[1])
+    if day_and_moth == int(date_list[2])%100:
+        return True
+    else:
+        return False
 
 # считываем данные
 date = input()
