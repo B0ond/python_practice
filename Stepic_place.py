f10e297ebab5b9604b1eee8c2f330266a86316fc
B0ond <amiram19@yandex.ru>
2023-11-22 21:13:45 +0300

    generete password update beta

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,6 +1,23 @@
 
-stroka1 = 'dddggg10oOhhh'
-stroka2 = 'il1Lo0Oo'
-translation_table = str.maketrans('', '', stroka2)
-out = stroka1.translate(translation_table)
-print(out)
+
+chars = ''
+while True:
+    count = 0
+    if validate[0]:
+        chars += digits
+        count += 1
+    if validate[1]:
+        chars += uppercase_letters
+        count += 1
+    if validate[2]:
+        chars += lowercase_letters
+        count += 1
+    if validate[3]:
+        chars += punctuation
+        count += 1
+    if count <= 0:
+        print('Пожалуйста выберите хотя бы один тип ')
+        validete_input()
+        continue
+    else:
+        break
--- Stepic_place_2.py
+++ Stepic_place_2.py
@@ -5,10 +5,10 @@ def get_input(prompt):
     """возвращает int число, цикл бесконечен до ввода числа"""
     while True:
         user_input = input(prompt)
-        if user_input.isdigit():
+        if user_input.isdigit() and int(user_input) >= 0:
             return int(user_input)
         else:
-            print('введите пожалуйста число')
+            print('введите пожалуйста корректное число')
             continue
 
 def yes_or_not(prompt):
@@ -21,12 +21,14 @@ def yes_or_not(prompt):
             return False
         else:
             print('Введите "Y" да или "N" нет')
+        break
 
 
 def generate_password(length, chars):
     """генератор паролей"""
     return ''.join(secrets.choice(chars) for _ in range(length))
-genereted_word = 'сгенерировано'
+
+
 
 def get_word_form(number):
     """меняет склонение слова <пароль>"""
@@ -51,28 +53,44 @@ def main():
     number_of_pass = get_input('Введите количество паролей для генерации: ')
     len_of_pass = get_input('Введите длину каждого пароля: ')
 
-    include_digit = yes_or_not('Включать ли цифры Y/N ')
-    includ_upper = yes_or_not('Включать ли прописные буквы Y/N ')
-    includ_lower = yes_or_not('Включать ли строчные буквы Y/N ')
-    includ_symbols = yes_or_not('Включать ли символы Y/N ')
-    indistinct = yes_or_not('Убрать из генерации неоднозначные символы Y/N ')
+    def validete_input():
+        include_digit = yes_or_not('Включать ли цифры Y/N ')
+        includ_upper = yes_or_not('Включать ли прописные буквы Y/N ')
+        includ_lower = yes_or_not('Включать ли строчные буквы Y/N ')
+        includ_symbols = yes_or_not('Включать ли символы Y/N ')
+        indistinct = yes_or_not('Убрать из генерации неоднозначные символы Y/N ')
+        return [include_digit, includ_upper, includ_lower, includ_symbols, indistinct]
 
-    if include_digit:
-        chars += digits
-    if includ_upper:
-        chars += uppercase_letters
-    if includ_lower:
-        chars += lowercase_letters
-    if includ_symbols:
-        chars += punctuation
-    if indistinct:
-        translate_table_indistinct = str.maketrans('', '', indistinct_Characters)  # maketrans создает таблицу словарь со значениями символов по табице ASKII
-        chars = chars.translate(translate_table_indistinct)  # translate превращает обратно в буквы
-    # if number_of_pass % 10 == 1:
-    #     genereted_word = 'сгенерирован'
-    # else:
-    #     gene
-    print(f'<<<Успешно сгенерировано {number_of_pass} {get_word_form(number_of_pass)} длиной {len_of_pass} символов>>>')
+    genereted_word = 'сгенерировано'
+
+    while True:
+        validate = validete_input()
+        count = 0
+        if validate[0]:
+            chars += digits
+            count += 1
+        if validate[1]:
+            chars += uppercase_letters
+            count += 1
+        if validate[2]:
+            chars += lowercase_letters
+            count += 1
+        if validate[3]:
+            chars += punctuation
+            count += 1
+        if validate[4]:
+            translate_table_indistinct = str.maketrans('', '', indistinct_Characters)  # maketrans создает таблицу словарь со значениями символов по табице ASKII
+            chars = chars.translate(translate_table_indistinct)  # translate превращает обратно в буквы
+        if number_of_pass % 10 == 1:
+            genereted_word = 'сгенерирован'
+        if count <= 0:
+            print('Пожалуйста выберите хотя бы один тип ')
+            validete_input()
+            continue
+        else:
+            break
+
+    print(f'<<<Успешно {genereted_word} {number_of_pass} {get_word_form(number_of_pass)} длиной {len_of_pass} символов>>>')
     for i in range(number_of_pass):
         print(generate_password(len_of_pass, chars))
 
