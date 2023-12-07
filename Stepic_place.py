045c8d5edede183672501f95ee85de0ce595b391
Amir <amiram19@yandex.ru>
2023-12-07 16:43:20 +0300

    caesar cipher 1.0.0

--- Stepic_place.py
+++ Stepic_place.py
@@ -1,8 +1,37 @@
-def get_declination(number_of_pass, len_of_pass):
-    text_genereted_word = 'сгенерировано'
-    text_symbol = 'символов'
-    if number_of_pass % 10 == 1:
-        text_genereted_word = 'сгенерирован'
-    if len_of_pass % 10 == 1:
-        text_symbol = 'символ'
-    return [text_genereted_word, text_symbol]
+print('шифр цезаря')
+def direction():
+    """
+    выбор шифровать или дешифровать
+    :return: True шифровать False дешифровать
+    """
+    x = input('Выберите "Ш" шифровать или "Д" дешифровать')
+    if x.lower() == 'ш':
+        return True
+    if x.lower() == 'д':
+        return False
+    print('Введите "Ш" шифровать или "Д" дешифровать')
+
+
+def text_lang():
+    """
+    выбор языка
+    :return: True русский False английский
+    """
+    x = input('Язык алфавита "р" русский, "а" английский')
+    if x.lower() == 'р':
+        return True
+    if x.lower() == 'а':
+        return False
+    print('Введите "р" русский, "а" английский')
+def caesar_cipher():
+    lower_eng_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    upper_eng_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower_rus_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+    upper_rus_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
