e0ed790336b5341040dc97b8a306039a108366ea
Amir <amiram19@yandex.ru>
2023-12-13 15:21:14 +0300

    caesar cipher 1.6.0

--- pet_projects/сaesars cipher/сaesar cipher.py
+++ pet_projects/сaesars cipher/сaesar cipher.py
@@ -17,6 +17,22 @@ def direction():
         print('Что то пошло не так 0_о: ')
 
 
+def known_or_not():
+    """
+    True - ключь неизвестен/False - ключь известен
+    :return: bool
+    """
+    while True:
+        y = input('Вы знаете ключь шифрования ? д/н ')
+        if y.lower() == 'н':
+            print('    >>>выбран поиск ключа')
+            return True
+        if y.lower() == 'д':
+            print('    >>>идите на следующий шаг')
+            return False
+        print('Что то пошло не так 0_о: ')
+
+
 def text_lang():
     """
     выбор языка c защитой
@@ -59,19 +75,11 @@ def step(text_lang_variable):
         print(f'Число должно быть положительным целым в пределах от 1 до {y}')
 
 
-def caesar_cipher(direction_variable, text_lang_variable, step_variable, text):
+def caesar_cipher_main(direction_variable, step_variable, text, abc, ABC, mosch):
     """
     функция шифрования
     :return:возвращает шифрованный или дешифрованный пароль
     """
-    if text_lang_variable is True:  # если True то Русский
-        abc = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
-        ABC = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-        mosch = 32
-    elif text_lang_variable is False:  # если False то Английский
-        abc = 'abcdefghijklmnopqrstuvwxyz'
-        ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        mosch = 26
     result = ''
 
     if direction_variable is True:  # если True то шифровать
@@ -90,23 +98,51 @@ def caesar_cipher(direction_variable, text_lang_variable, step_variable, text):
                 result += ABC[(ABC.find(i) - step_variable) % mosch]
             else:
                 result += i
-
     return result
+
+
+def find_key(text, spell, direction_variable, abc, ABC, mosch):
+    """рашифровка ключа"""
+    key_arr = []
+    for key in range(1, len(text)-1):  # хз почему от 1 до text-1
+        decoded_text = caesar_cipher_main(direction_variable, key, text, abc, ABC, mosch)
+        # в переменную входит расшифровка с клчюом key
+        decoded_text_arr = decoded_text.split()  # делаем список из слов
+        len_decoded_text_arr = len(decoded_text_arr)  # длина списка
+        res = 0  # переменная для подсчета количества вхождений
+        for j in range(len_decoded_text_arr):  # пробегаемся по словам
+            if decoded_text_arr[j] in spell:  # если слово правильное орфографически
+                res += 1                      # то записываем в res
+        key_arr.append(res)  # создаем список из количества найденных правильных слов
+    max_key = max(key_arr)
+    return key_arr.index(max_key)
 
 
 def main():
     """main function"""
+    global step_variable, direction_variable, abc, ABC, mosch, spell
     print("Caeser's cipher")  # шапка
-    direction_variable = direction()  # направление свига
-    text_lang_variable = text_lang()# выбранный язык
-    if text_lang_variable:
-        spell = SpellChecker(language='ru')
-    else:
-        spell = SpellChecker(language='en')
-    step_variable = step(text_lang_variable)  # сдвиг
-    text = input('Введите текст: ')  # текст для обработки
-    print(caesar_cipher(direction_variable, text_lang_variable, step_variable, text))  #вывод функции шифрования
 
+    known_or_not_variable = known_or_not()
+    if known_or_not_variable is False:
+        direction_variable = direction()  # направление свига
+    text_lang_variable = text_lang()  # выбранный язык
+    if text_lang_variable is True:  # если True то Русский
+        abc = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+        ABC = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+        spell = SpellChecker(language='ru')
+        mosch = 32
+    elif text_lang_variable is False:  # если False то Английский
+        abc = 'abcdefghijklmnopqrstuvwxyz'
+        ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        spell = SpellChecker(language='en')
+        mosch = 26
+    if known_or_not_variable is False:
+        step_variable = step(text_lang_variable)  # сдвиг
+    text = input('Введите текст: ')  # текст для обработки
+    if known_or_not_variable is False:
+        print(caesar_cipher_main(direction_variable, step_variable, text, abc, ABC, mosch))  #вывод функции шифрования
+    print(f' ключь равен {find_key(text, spell, direction_variable, abc, ABC, mosch)}')
 
 if __name__ == '__main__':
     main()
