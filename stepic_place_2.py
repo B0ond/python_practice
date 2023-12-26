7b2d1675a0e15f2482f6e2ecc4ddc97072442a18
Amir <amiram19@yandex.ru>
2023-12-26 19:21:05 +0300

    update 11

--- stepic_place_2.py
+++ stepic_place_2.py
@@ -1,25 +1,46 @@
-class Flat:
-    """Класс квартир в микрорайоне залупинск"""
-    street = 'Gorkogo'
-    street_number = 7
-    flat_number = 5
-    floor = 3
+# class Flat:
+#     """Класс квартир в микрорайоне залупинск"""
+#     street = 'Gorkogo'
+#     street_number = 7
+#     flat_number = 5
+#     floor = 3
+#
+#     def set_cost(self, first, second):
+#         self.first = first
+#         self.second = second
+#
+#     def get_cost(self):
+#         return (self.first, self.second)
+#
+#
+# pervichka_cena = Flat()
+# per = Flat()
+# per.set_cost(7000, 5000)
+# per_ret = per.get_cost()
+#
+#
+# pervichka_cena.set_cost(5000, 4500)
+# pervichka_return = pervichka_cena.get_cost()
+#
+# print(*pervichka_return, list(per_ret))
 
-    def set_cost(self, first, second):
-        self.first = first
-        self.second = second
+class Animals:
+    ban_name = 'Foggy'
+    min_age = 1
 
-    def get_cost(self):
-        return (self.first, self.second)
+    @classmethod
+    def validate_name(cls, arg):
+        return cls.ban_name
+    def __init__(self, x: str, y: int):
+        self.x = x
+        self.y = y
+
+    def get_age(self):
+        return self.y
+
+    def get_name(self):
+        return self.x
 
 
-pervichka_cena = Flat()
-per = Flat()
-per.set_cost(7000, 5000)
-per_ret = per.get_cost()
-
-
-pervichka_cena.set_cost(5000, 4500)
-pervichka_return = pervichka_cena.get_cost()
-
-print(*pervichka_return, list(per_ret))
+dog = Animals('Sunny', 2)
+print(f'Имя собаки {dog.get_name()} ее возраст {dog.get_age()}')
