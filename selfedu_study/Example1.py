659708765e0962e7bd70925947410737b3aedb8d
Amir <amiram19@yandex.ru>
2024-01-12 13:53:55 +0300

    Update! lesson 14 have a question

--- selfedu_study/Example1.py
+++ selfedu_study/Example1.py
@@ -1,102 +1,45 @@
-from string import ascii_letters
+class Clock:
+    __DAY = 86400
 
+    def __init__(self, second: int):
+        if not isinstance(second, int):
+            raise TypeError("Секунды должны быть целым числом")
+        self.seconds = second % self.__DAY
 
-class Person:
-    S_RUS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя-"
-    S_RUS_UPPER = S_RUS.upper()
-
-    def __init__(self, fio: str, age: int, passport: str, weight: float):
-        self.verify_fio(fio)
-        # self.verify_age(age)
-        # self.verify_pasport(passport)
-        # self.verify_weight(weight)
-
-        self.__fio = fio  # str
-        self.age = age  # int 14 <= fio <= 120
-        self.passport = passport  # format xxxx xxxxxx, where (x: int) == (0 <= int <= 9)
-        self.weight = weight  # float > 20.0 kg
+    def get_time(self):
+        s = self.seconds % 60
+        m = (self.seconds // 60) % 60
+        h = (self.seconds // 3600) % 24
+        return f"{self.__get_formatted(h)}:{self.__get_formatted(m)}:{self.__get_formatted(s)}"
 
     @classmethod
-    def verify_fio(cls, fio):
-        if type(fio) != str:
-            raise TypeError('ФИО должно быть строкой')
-        f = fio.split()
-        if len(f) != 3:
-            raise TypeError("Неверный формат записи")
+    def __get_formatted(cls, x):
+        return str(x).rjust(2, "0")
 
-        letters = ascii_letters + cls.S_RUS + cls.S_RUS_UPPER
-        for letter in f:
-            if len(letter) < 1:
-                raise TypeError("В ФИО должна быть хотя бы 1 буква")
-            if len(letter.strip(letters)) != 0:
-                raise TypeError("В ФИО можно использвоать буквенные символы и девис")
+    def __add__(self, other):
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Правый операнд должен быть целым числом")
+        sc = other
+        if isinstance(other, Clock):
+            sc = other.seconds
+        return Clock(self.seconds + sc)
 
-    # @classmethod
-    # def verify_age(cls, age):
-    #     if type(age) != int or 14 > age > 120:
-    #         raise TypeError("Возраст должен быть от 14 до 120 в цифрах")
+    def __radd__(self, other):
+        return self + other
 
-    @classmethod
-    def verify_age(cls, age):
-        if type(age) != int or 14 > age or age > 120:
-            raise TypeError("Возраст должен быть от 14 до 120 в цифрах")
-
-    @classmethod
-    def verify_weight(cls, weight):
-        if type(weight) != float or weight < 20:
-            return TypeError("Вес должен быть вещественным числом от 20 и выше")
-
-    @classmethod
-    def verify_pasport(cls, passport):  # с хера не работает класс как исключение! наверное не вызвал! потом решу!
-        if type(passport) != str:
-            raise TypeError("Паспорт должен быть строкой")
-        s = passport.split()
-        if len(s) != 2 or len(s[0]) != 4 or len(s[1]) != 6:
-            raise TypeError("Неверный формат паспорта")
-        for p in s:
-            if not p.isdigit():
-                raise TypeError("Ссерия и номер паспорта должны быть числами")
-
-    @property
-    def fio(self):
-        return self.__fio
-
-    @fio.setter
-    def fio(self, fio):
-        self.verify_fio(fio)
-        self.__fio = fio
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age):
-        self.verify_age(age)
-        self.__age = age
-
-    @property
-    def passport(self):
-        return self.__passport
-
-    @passport.setter
-    def passport(self, passport):
-        self.verify_pasport(passport)
-        self.__passport = passport
-
-    @property
-    def weight(self):
-        return self.__weight
-
-    @weight.setter
-    def weight(self, weight):
-        self.verify_weight(weight)
-        self.__weight = weight
+    def __iadd__(self, other):
+        print("__iadd__")
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Правый операнд должен быть int или Clock")
+        sc = other
+        if isinstance(other, Clock):
+            sc = other.seconds
+        self.seconds += sc
+        return self
 
 
-x = Person("Ялалтдинов Амирам Мухарьямович", 27, '4457 365678', 78.7)
-x.fio = 'Ялаев Филюс Мулланурович'
-x.age = 29
-x.passport = '1111 666664'
-x.weight = '85'
-print(x.__dict__)
+c1 = Clock(1000)
+c2 = Clock(2000)
+c1 = c1 + 100
+print(c1.seconds)
+print(c1.get_time())
--- stepic_place.py
+++ stepic_place.py
@@ -1,26 +1,12 @@
-# дан массив неупорядочных чисел [1, 2, 4, 12, 6, 3, 7, 90, 5, 2, 0, 4] и число k
-# нужно найти число из массива в сумме дающие k
+class Time:
+    __DAY = 86400
 
+    def __init__(self, second):
+        if not isinstance(second, int):
+            raise TypeError("Должно быть целое число")
+        self.seconds = second
 
-class Solution:
-    @staticmethod
-    def find_sum_index(arr: list[int], k: int) -> list[int]:
-        set_num = {}
-        for idx, values in enumerate(arr):
-            if k - values in set_num:
-                return [set_num[k - values], idx]
-            set_num[values] = idx
-
-    @staticmethod
-    def find_sum(arr: list[int], k: int) -> list[int]:
-        set_num = []
-        for values in arr:
-            if k - values in set_num:
-                return [values, k-values]
-            set_num.append(values)
-
-
-arr_values = [1, 20, 4, 12, 6, 70, 7, 90, 5, 2, 0, 4]
-k_values = 9
-print(f"Индекс искомых хначений: {Solution.find_sum_index(arr_values, k_values)}")
-print(f"Сами числа: {Solution.find_sum(arr_values, k_values)}")
+    def get_time(self):
+        s = self.seconds % 60
+        m = (self.seconds // 60) % 60
+        h = (self.seconds // 3600) % 24
