1f5e50724d9e9452ee0b1c3962cd9e365c87e687
Amir <amiram19@yandex.ru>
2024-06-27 16:07:22 +0300

    вот так вот

--- file1.py
+++ file1.py
@@ -1,14 +1,3 @@
-vowels = ['num', 'e', 'i', 'o', 'u']
-word = input('Insert num word: ')
-found = []
-counter = {}
-for letter in word:
-    if letter in vowels:
-        counter[letter] = counter.get(letter, 0) + 1
-        found.append(letter)
-print(set(found))
+# нужно сделать стак у которого ест ьметоды
 
-#         if letter not in found:
-#             found.append(letter)
-# for i, j in zip(found, counter.count(k) for k in counter):
-#     print(i, j)
+class Stack:
