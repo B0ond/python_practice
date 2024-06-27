0f0788087f2602bbf237c1c91cd52595efd57ea3
Amir <amiram19@yandex.ru>
2024-06-27 16:15:26 +0300

    test task

--- file1.py
+++ file1.py
@@ -1,3 +1,13 @@
-# нужно сделать стак у которого ест ьметоды
+# нужно сделать стак у которого есть методы push(вносим в данные), pop(удаляем и возвращаем), current(текущее значение),
+# top(максимальное значение), min_value(минимальное значение)
 
 class Stack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        if not self.min_stack or x < self.min_stack[-1]:
+            self.min_stack.append(x)
+
