30c21af14a387e4ee9f3d183be7d4dc3074306f8
Amir <amiram19@yandex.ru>
2024-06-28 14:27:48 +0300

    test task2

--- file1.py
+++ file1.py
@@ -1,13 +1,40 @@
-# нужно сделать стак у которого есть методы push(вносим в данные), pop(удаляем и возвращаем), current(текущее значение),
-# top(максимальное значение), min_value(минимальное значение)
+# # нужно сделать стак у которого есть методы push(вносим в данные), pop(удаляем и возвращаем),
+# # top(верхнее значение), min_value(минимальное значение)
+#
+# from array import array
+#
+#
+# class Stack:
+#     def __init__(self):
+#         self.data = []
+#         self.min_data = []
+#
+#     def push(self, num):
+#         self.data.append(num)
+#         if not self.min_data or num <= self.min_data[-1]:
+#             self.min_data.append(num)
+#
+#     def pop(self):
+#         if not self.data:
+#             return None
+#         removed_element = self.data.pop()
+#         if removed_element == self.min_data[-1]:
+#             self.min_data.pop()
+#         return removed_element
+#
+#     def top(self):
+#         if self.data:
+#             return self.data[-1]
+#
+#     def min_value(self):
+#         if self.min_data:
+#             return self.min_data[-1]
+#
+#
+# out = Stack()
 
-class Stack:
-    def __init__(self):
-        self.stack = []
-        self.min_stack = []
+def modif_str(stroka:str):
+    return stroka[1:-1]
 
-    def push(self, x):
-        self.stack.append(x)
-        if not self.min_stack or x < self.min_stack[-1]:
-            self.min_stack.append(x)
 
+print(modif_str('A1234567A'))
